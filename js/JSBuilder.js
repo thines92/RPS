dc608fbc9419c8e0bf915cd19df24a23fb72ce83
Tyler <tylerhines1992@gmail.com>
2015-08-07 10:07:39 -0400

    updated ui

--- js/JSBuilder.js
+++ js/JSBuilder.js
@@ -1,41 +1,37 @@
+function rps() {
+	var userChoice = prompt("Please choose: Rock, Paper or Scissors?").toLowerCase();
 
-var userChoice = prompt("Please choose: Rock, Paper or Scissors?").toLowerCase();
+	var computerChoice = Math.random();
+	var computerPick;
 
-var computerChoice = Math.random();
-var computerPick;
+	if (computerChoice < .33) {
+		var computerPick = "rock";
+	}
+	if (computerChoice >= .33 || computerChoice < .66) {
+		var computerPick = "paper";
+	}
+	if (computerChoice >= .66) {
+		var computerPick = "scissors";
+	};
 
-if (computerChoice < .33) {
-	var computerPick = "rock";
+	if (userChoice === computerPick) {
+		document.getElementById("result").innerHTML = "The result is a tie!";
+	}
+	else if (userChoice === "rock" && computerPick === "paper") {
+		document.getElementById("result").innerHTML = "The computer wins!";
+	}
+	else if (userChoice === "paper" && computerPick === "scissors") {
+		document.getElementById("result").innerHTML = "The computer wins!";
+	}
+	else if (userChoice === "scissors" && computerPick === "rock") {
+		document.getElementById("result").innerHTML = "The computer wins!";
+	}
+	else {
+		document.getElementById("result").innerHTML = "You win!";
+	};
+
+/*	document.getElementById("result").innerHTML = computerPick;
+	document.getElementById("result").innerHTML = userChoice;*/
 }
-if (computerChoice >= .33 || computerChoice < .66) {
-	var computerPick = "paper";
-}
-if (computerChoice >= .66) {
-	var computerPick = "scissors";
-};
 
-if (userChoice === computerPick) {
-	document.write("The result is a tie!");
-	userChoice = prompt("Please choose: Rock, Paper or Scissors?").toLowerCase();
-}
-else if (userChoice === "rock" && computerPick === "paper") {
-	document.write("The computer wins!");
-}
-else if (userChoice === "paper" && computerPick === "scissors") {
-	document.write("The computer wins!");
-}
-else if (userChoice === "scissors" && computerPick === "rock") {
-	document.write("The computer wins!");
-}
-else {
-	document.write("You win!");
-};
-
-
-
-
-//document.write(userChoice);
-//document.write(computerPick);
-
-
-
+document.getElementById("callhere").onclick = function() {rps()};
